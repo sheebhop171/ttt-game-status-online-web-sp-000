a618757c7643667925815b639256f2e5fd0991dc
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-04-11 16:19:01 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,7 +4,7 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
-def WIN_COMBINATIONS = [
+WIN_COMBINATIONS = [
   [0,1,2]
   [3,4,5]
   [6,7,8]
@@ -15,4 +15,3 @@ def WIN_COMBINATIONS = [
   [2,4,6]
 ]
 
-end
