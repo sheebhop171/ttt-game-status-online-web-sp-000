cbc1174f0f670b846f9a240d2bbff3e9863c8896
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-04-11 17:12:08 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -25,3 +25,8 @@ def won?(board)
     board[location1] == board[location2] && board[location2] == board[location3] && board[location1] != " "
   end
 end
+
+def full?(board)
+  board.any { | position | position == " "}
+  
+end
